70d019213b576d85c467fa8b1a04b8c3808b05ac
Romain Portanguen <portanguenr@gmail.com>
2024-07-25 12:59:51 +0200

    Removing the Jest configuration

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
-};
